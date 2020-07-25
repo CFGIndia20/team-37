3210dd517f823e83016a93e33e55528ed1148c65
Amodh Akhelikar <amodh10@gmail.com>
2020-07-25 19:00:38 +0530

    Chatbot integration

--- stjude/chatbot/urls.py
+++ stjude/chatbot/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get-response/', views.get_response, name='get_response'),
 ]
--- stjude/chatbot/views.py
+++ stjude/chatbot/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from chatbot.chatbot_model_runner import *
+
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(request, "home.html")
+    return render(request, "home.html")
 
 @csrf_exempt
 def get_response(request):
@@ -15,9 +16,9 @@ def get_response(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         message = data['message']
-        print(message)
+        # print(message)
 
-        chat_response = "HI"
+        chat_response = chatbot_response(message)
         response['message'] = {'text': chat_response, 'user': False, 'chat_bot': True}
         response['status'] = 'ok'
 
@@ -29,6 +30,4 @@ def get_response(request):
         content_type = "application/json"
     )
 
-def home(request, template_name="home.html"):
-    context = {'title': 'TEMP'}
-    return render(template_name, context)
+
