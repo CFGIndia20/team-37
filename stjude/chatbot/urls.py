ca2e1bd2e83be9718d518f37e4b6b7c66071e0a9
rksneha <suchithra.295@gmail.com>
2020-07-26 07:26:19 +0530

    new_update

--- stjude/chatbot/urls.py
+++ stjude/chatbot/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('option-chosen/', views.optionChosen),
     path('admin-options/', views.optionChosen),
     path('get-response/', views.get_response, name='get_response'),
+    path('view-patient/',views.view_patient),
+    path('checkout-patient/',views.checkout)
 ]
